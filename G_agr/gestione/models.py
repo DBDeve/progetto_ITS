7c5bca7ef99636108b82721962658ebb4c10c545
dario bertonati <dario.bertonatilavoro@gmail.com>
2024-04-18 17:19:16 +0200

    aggiornamento models.py

--- G_agr/gestione/models.py
+++ G_agr/gestione/models.py
@@ -112,9 +112,9 @@ class Salary(models.Model):
 class Clients(models.Model):
   #collegamenti superiori
   IdAccountManagers=models.ForeignKey(AccountManagers, on_delete=models.CASCADE)
-  IdFarmHouses=models.ForeignKey(FarmHouses, on_delete=models.CASCADE)
-  ClientRoom=models.OneToOneField(Rooms, on_delete=models.CASCADE)
-  ClientServices = models.ForeignKey(Services, on_delete=models.CASCADE,default=0)
+  IdFarmHouses=models.ForeignKey(FarmHouses, on_delete=models.CASCADE,null=True, blank=True)
+  ClientRoom=models.OneToOneField(Rooms, on_delete=models.CASCADE,null=True, blank=True)
+  ClientServices = models.ForeignKey(Services, on_delete=models.CASCADE,default=0,null=True, blank=True)
   #dati tabella
   name=models.CharField(default="",max_length=255,help_text = "*")
   mail=models.EmailField(default="", help_text = "*")
