0229c92ca744509fc6112f77421f707a5bd3444a
dario bertonati <dario.bertonatilavoro@gmail.com>
2024-04-24 20:05:53 +0200

    aggiornamento funzione visualizza() e aggiungi() con relativi templates

--- G_agr/gestione/views.py
+++ G_agr/gestione/views.py
@@ -55,7 +55,7 @@ def visualizza(request,username,agriturismo,argomento,scelta):
          agriturismo_id=agriturismo.id
          if scelta=="tutte":
             for classe in classi_agriturismi:
-               context[f'valori_{classe.__name__}']=classe.objects.filter(IdFarmHouses=agriturismo_id)
+               context[f'valori_{classe.__name__}']=classe.objects.filter(IdFarmHouses=agriturismo_id,)
          elif scelta=="occupata": 
             context[f'valori_Clients']=Clients.objects.all().values()
             context[f'Rooms_occupata']=Rooms.objects.filter(stato=scelta, IdFarmHouses_id=agriturismo_id)
@@ -74,7 +74,7 @@ def visualizza(request,username,agriturismo,argomento,scelta):
 
 #la funzione aggiungi crea nuovo oggetti in base all'argomento e l'username che gli viene passato
 @login_required(login_url='login')
-def aggiungi(request,username,agriturismo,argomento,):
+def aggiungi(request,username,agriturismo,argomento):
    context={}
    context['argomento']=argomento
    context['username']=username
@@ -84,11 +84,10 @@ def aggiungi(request,username,agriturismo,argomento,):
    user_id=user.id
    accounts=AccountManagers.objects.get(gestore_id=user_id)
    accounts_id=accounts.id
-   agriturismo=FarmHouses.objects.get(IdAccountManagers_id=accounts_id, FarmHouseName=agriturismo)
-   agriturismo_id=agriturismo.id
+   agriturismo_s=FarmHouses.objects.get(IdAccountManagers_id=accounts_id)
+   agriturismo_id=agriturismo_s.id
 
 
-   
    if request.POST:
       #crea un nuovo oggetto Rooms (funziona)
       if argomento=="camere": 
@@ -101,6 +100,25 @@ def aggiungi(request,username,agriturismo,argomento,):
             IdFarmHouses_id=agriturismo_id
             )
          nuova_camera.save()
+         url=f"/gestione/visualizza/{username}/{agriturismo}/{argomento}/tutte"
+         return redirect(url) 
+      if argomento=="promozioni":
+         nuova_promozione=Promotions(
+            name=request.POST['name'],
+            IdFarmHouses_id=agriturismo_id
+         )
+         nuova_promozione.save()
+         url=f"/gestione/visualizza/{username}/{agriturismo}/{argomento}/tutte"
+         return redirect(url) 
+      if argomento=="servizzi":
+         nuovo_servizio=Services(
+            name=request.POST['name'],
+            prize=request.POST['prize'],
+            IdFarmHouses_id=agriturismo_id
+         )
+         nuovo_servizio.save()
+         url=f"/gestione/visualizza/{username}/{agriturismo}/{argomento}/tutte"
+         return redirect(url) 
       #crea nuovo aggetto Clients (non funziona "integrity error" quardare modello)
       if argomento=="clienti":
          nuovo_cliente=Clients(
@@ -111,8 +129,10 @@ def aggiungi(request,username,agriturismo,argomento,):
             IdFarmHouses_id=agriturismo_id
             )
          nuovo_cliente.save()
+         url=f"/gestione/visualizza/{username}/{agriturismo}/{argomento}/tutte"
+         return redirect(url) 
       # crea nuovo oggetto employee (funziona)
-      elif argomento=="dipendenti":
+      if argomento=="dipendenti":
          nuovo_lavoratore=Employee(
             name=request.POST['name'],
             codice_fiscale=request.POST['codice_fiscale'],
@@ -122,6 +142,37 @@ def aggiungi(request,username,agriturismo,argomento,):
             IdFarmHouses_id=agriturismo_id
          )
          nuovo_lavoratore.save()
+         url=f"/gestione/visualizza/{username}/{agriturismo}/{argomento}/tutte"
+         return redirect(url) 
+      
+      if argomento=="uscite":
+         nuova_uscita=Expense(
+            Date=request.POST['date'],
+            Quantity=request.POST['quantity'],
+            IdAccountManagers_id=accounts_id,
+         )
+         nuova_uscita.save()
+         url=f"/gestione/visualizza/{username}/{agriturismo}/{argomento}/tutte"
+         return redirect(url) 
+      
+      if argomento=="entrate":
+         nuova_entrata=Earnings(
+            Date=request.POST['date'],
+            Quantity=request.POST['quantity'],
+            IdAccountManagers_id=accounts_id,
+         )
+         nuova_entrata.save()
+         url=f"/gestione/visualizza/{username}/{agriturismo}/{argomento}/tutte"
+         return redirect(url) 
+      if argomento=="agriturismo":
+         nuovo_agriturismo=FarmHouses(
+            FarmHouseName=request.POST['name'],
+            address=request.POST['address'],
+            IdAccountManagers_id=accounts_id
+            )
+         nuovo_agriturismo.save() 
+         url=f"/gestione/visualizza/{username}/{agriturismo}/{argomento}/tutte"
+         return redirect(url) 
       
    template=loader.get_template('form_aggiungi.html')
    return HttpResponse(template.render(context,request))      
@@ -156,7 +207,7 @@ def modifica(request,argomento,username,agriturismo,valore):
          #salvataggio dell'oggetto con i dati modificati
          camera_da_modificare.save()
          #ridirezionamento alla fine della modifica
-         url=f"/gestione/visualizza/{username}/camere/tutte"
+         url=f"/gestione/visualizza/{username}/{agriturismo}/camere/tutte"
          return redirect(url)
      #selezione dei dati da modificare
      
@@ -200,7 +251,7 @@ def elimina(request,username,agriturismo,argomento,valore):
             number=valore
             camera_da_rimuovere=Rooms.objects.get(number=number,IdFarmHouses=agriturismo_id)
             camera_da_rimuovere.delete()
-            url=f"http://127.0.0.1:8000/gestione/visualizza/{username}/{agriturismo}/{argomento}/tutte"
+            url=f"/gestione/visualizza/{username}/{agriturismo}/{argomento}/tutte"
             return redirect(url)
          elif argomento=="clienti":
             name=request.POST['name']
@@ -308,7 +359,8 @@ def registrati(request):
 
       #rendirizza alla pagina di login
       url=f"login"
-      return redirect(url)
+      return render(request, "form_accedi.html", {"argomento":'log in | accedi'})
+
 
    template=loader.get_template('form_registrati.html')
    return HttpResponse(template.render(context,request))
