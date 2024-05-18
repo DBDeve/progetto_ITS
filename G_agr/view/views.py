f6c5f0f071a7755cb1c9a676113a612e6c2a57d8
dario bertonati <dario.bertonatilavoro@gmail.com>
2024-05-18 02:30:32 +0200

    modifica applicazione view: templates, views.py

--- G_agr/view/views.py
+++ G_agr/view/views.py
@@ -17,7 +17,7 @@ def visualizza_per_attivita(request,username,agriturismo,attivita,filtro):
     context['attivita']=attivita
     context['filtro']=filtro
     
-    
+    #filtra le informazioni
     user=User.objects.get(username=username)
     user_id=user.id
     account=AccountManagers.objects.get(gestore_id=user_id)
@@ -27,14 +27,14 @@ def visualizza_per_attivita(request,username,agriturismo,attivita,filtro):
     attivita=Activity.objects.get(IdFarmHouses=agriturismo_id, ActivityName=attivita)
     attivita_id=attivita.id
     
+    #crea una query con tutte le attività legate all'agriturismo
+    context['attivita']=Activity.objects.filter(IdFarmHouses=agriturismo_id)
     
+    #crea delle query degli oggetti legati alle attività
     context['entrate_attivita']=Earnings.objects.filter(IdActivity_id=attivita_id)
     context['uscite_attivita']=Expense.objects.filter(IdActivity_id=attivita_id)
     context['clienti_attivita']=Clients.objects.filter(IdActivity_id=attivita_id)
     context['lavoratori_attivita']=Employee.objects.filter(IdActivity_id=attivita_id)
-
+    
+    #manda il contenuto delle query alla pagine "visualizza_per_attivita.html"
     return render(request, "visualizza_per_attivita.html", context)
-
-    
-    
-    #return render(request, "gestione_oggetto_singolo.html", context)
